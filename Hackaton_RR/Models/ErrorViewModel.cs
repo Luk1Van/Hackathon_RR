14da4fcb83bae3f47e1c3f2988af5c1d72efa20f
иван лукич <ivkurcic@gmail.com>
2023-09-09 11:14:48 +0300

    Add project files.

---
+++ Hackaton_RR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hackaton_RR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
